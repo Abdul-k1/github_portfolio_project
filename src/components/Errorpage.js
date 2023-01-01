39f7969e78d652e415ea47e7c0d1075c370fd7d0
Abdul-k1 <odiabdulkareem@gmail.com>
2023-01-01 01:28:28 +0100

    made changes to ErrorBoundary

--- src/components/Errorpage.js
+++ src/components/Errorpage.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div className="error-page">
-            <h1>This is an Error Page</h1>
-            <Link to="/">
-                <button className="home-btn">Go to Homepage</button>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="error-page">
+      <h1>This is an Error Page</h1>
+
+      <Link to="/">
+        <button className="home-btn">Go to Homepage</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Error;
--- src/components/TestBoundary.js
+++ src/components/TestBoundary.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Profile from "./Profile";
+import ErrorBoundary from "./ErrorBoundary";
+import { Link } from "react-router-dom";
 
 const TestBoundary = () => {
-    /* eslint-disable */
-    throw Error();
-    return (
-        <div>
-           <Profile /> 
-        </div>
-    )
-}
+  return (
+    <div className="error-page">
+      {/* Implementing Test Boundary  */}
+      <ErrorBoundary>
+        <ErrorComponent />
+        <Profile />
+      </ErrorBoundary>
+      <Link to="/">
+        <button className="home-btn">Go to Homepage</button>
+      </Link>
+    </div>
+  );
+};
+
+const ErrorComponent = () => {
+  throw new Error();
+};
 
 export default TestBoundary;
